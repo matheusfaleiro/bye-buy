a3fbdd147c284be2ac8d0ca63a620ea92c485bae
Matheus Faleiro <me@matheusfaleiro.dev>
2025-10-19 10:43:18 +0200

    Add Home Feature Module
    Create home feature module to display the user's list of items
    they are considering purchasing. This module serves as the main
    hub where users can review their potential purchases and make
    conscious buying decisions.
    
    Signed-off-by: Matheus Faleiro <me@matheusfaleiro.dev>

---
+++ feature-home/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.library)
+
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.detekt)
+
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+}
+
+android {
+    namespace = "dev.matheusfaleiro.featurehome"
+
+    compileSdk {
+        version = release(version = 36)
+    }
+
+    defaultConfig {
+        minSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    kotlin {
+        jvmToolchain(libs.versions.jvm.target.get().toInt())
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
